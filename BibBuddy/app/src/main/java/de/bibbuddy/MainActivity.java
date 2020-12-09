c40213a75fad01613e46916d5d6f6fbb3bc8a32b
claudia3 <claudia13019@gmail.com>
2020-12-09 14:48:45 +0100

    Leere Activity hinzugefügt

---
+++ BibBuddy/app/src/main/java/de/bibbuddy/MainActivity.java
@@ -0,0 +1,15 @@
+package de.bibbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+   }
+   // Home Search Library Notes
+}
