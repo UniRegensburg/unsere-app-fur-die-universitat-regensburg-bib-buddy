c1e29ab2432b8eb572bf85fda2ea38333c16ae22
Sabrina Freisleben <41908872+xSabrina@users.noreply.github.com>
2021-02-08 14:00:29 +0100

    Bugfix: empty text notes are not saved anymore
    Fix a bug where textNotes were saved even if they only contain empty space (linebreaks etc, without any raw text). The check for like text is now checking only the raw text using jsoup.

--- BibBuddy/app/src/main/java/de/bibbuddy/TextNoteEditorFragment.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/TextNoteEditorFragment.java
@@ -11,300 +11,302 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import org.jsoup.Jsoup;
 
 public class TextNoteEditorFragment extends Fragment {
 
-	private Long noteId;
-	private View view;
-	private RichTextEditor richTextEditor;
-	private boolean highlighted = false;
-	private Note note;
-	private NoteModel noteModel;
 
-	@Nullable
-	@Override
-	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-													 @Nullable Bundle savedInstanceState) {
-		view = inflater.inflate(R.layout.fragment_text_note_editor, container, false);
-		richTextEditor = view.findViewById(R.id.editor);
-		noteModel = new NoteModel(getContext());
-		if (getArguments() != null) {
-			noteId = getArguments().getLong("noteId");
-			note = noteModel.getNoteById(noteId);
-			richTextEditor.setText(Html.fromHtml(note.getText(), 33));
-		} else {
-			note = noteModel.newNote();
-		}
-		richTextEditor.setSelection(richTextEditor.getEditableText().length());
-		setupUndo();
-		setupRedo();
-		setupBold();
-		setupItalic();
-		setupUnderline();
-		setupStrikethrough();
-		setupBullet();
-		setupQuote();
-		setupAlignment();
-		initSlidingButtons();
-		return view;
-	}
+  private View view;
+  private RichTextEditor richTextEditor;
+  private boolean highlighted = false;
+  private Note note;
+  private NoteModel noteModel;
 
-	private void highlightSelectedItem(View view) {
-		if (!highlighted) {
-			view.setBackgroundColor(getActivity().getColor(R.color.flirt_light));
-			highlighted = true;
-		} else {
-			view.setBackgroundColor(0);
-			highlighted = false;
-		}
-	}
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                           @Nullable Bundle savedInstanceState) {
+    view = inflater.inflate(R.layout.fragment_text_note_editor, container, false);
+    richTextEditor = view.findViewById(R.id.editor);
+    noteModel = new NoteModel(getContext());
+    if (getArguments() != null) {
+      Long noteId = getArguments().getLong("noteId");
+      note = noteModel.getNoteById(noteId);
+      richTextEditor.setText(Html.fromHtml(note.getText(), 33));
+    } else {
+      note = noteModel.newNote();
+    }
+    richTextEditor.setSelection(richTextEditor.getEditableText().length());
+    setupUndo();
+    setupRedo();
+    setupBold();
+    setupItalic();
+    setupUnderline();
+    setupStrikethrough();
+    setupBullet();
+    setupQuote();
+    setupAlignment();
+    initSlidingButtons();
+    return view;
+  }
 
-	private void setupUndo() {
-		ImageButton undo = view.findViewById(R.id.action_undo);
-		undo.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.undo();
-				highlightSelectedItem(undo);
-			}
-		});
-	}
+  private void highlightSelectedItem(View view) {
+    if (!highlighted) {
+      view.setBackgroundColor(getActivity().getColor(R.color.flirt_light));
+      highlighted = true;
+    } else {
+      view.setBackgroundColor(0);
+      highlighted = false;
+    }
+  }
 
-	private void setupRedo() {
-		ImageButton redo = view.findViewById(R.id.action_redo);
-		redo.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.redo();
-				highlightSelectedItem(redo);
-			}
-		});
-	}
+  private void setupUndo() {
+    ImageButton undo = view.findViewById(R.id.action_undo);
+    undo.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.undo();
+        highlightSelectedItem(undo);
+      }
+    });
+  }
 
-	private void setupBold() {
-		ImageButton bold = view.findViewById(R.id.action_bold);
+  private void setupRedo() {
+    ImageButton redo = view.findViewById(R.id.action_redo);
+    redo.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.redo();
+        highlightSelectedItem(redo);
+      }
+    });
+  }
 
-		bold.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.bold(richTextEditor.contains(RichTextEditor.FORMAT_BOLD));
-				highlightSelectedItem(bold);
-			}
-		});
+  private void setupBold() {
+    ImageButton bold = view.findViewById(R.id.action_bold);
 
-		bold.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Toast.makeText(getContext(), R.string.toast_bold, Toast.LENGTH_SHORT).show();
-				return true;
-			}
-		});
-	}
+    bold.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.bold(richTextEditor.contains(RichTextEditor.FORMAT_BOLD));
+        highlightSelectedItem(bold);
+      }
+    });
 
-	private void setupItalic() {
-		ImageButton italic = view.findViewById(R.id.action_italic);
+    bold.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View v) {
+        Toast.makeText(getContext(), R.string.toast_bold, Toast.LENGTH_SHORT).show();
+        return true;
+      }
+    });
+  }
 
-		italic.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.italic(richTextEditor.contains(RichTextEditor.FORMAT_ITALIC));
-				highlightSelectedItem(italic);
-			}
-		});
+  private void setupItalic() {
+    ImageButton italic = view.findViewById(R.id.action_italic);
 
-		italic.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Toast.makeText(getContext(), R.string.toast_italic, Toast.LENGTH_SHORT).show();
-				return true;
-			}
-		});
-	}
+    italic.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.italic(richTextEditor.contains(RichTextEditor.FORMAT_ITALIC));
+        highlightSelectedItem(italic);
+      }
+    });
 
-	private void setupUnderline() {
-		ImageButton underline = view.findViewById(R.id.action_underline);
+    italic.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View v) {
+        Toast.makeText(getContext(), R.string.toast_italic, Toast.LENGTH_SHORT).show();
+        return true;
+      }
+    });
+  }
 
-		underline.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.underline(richTextEditor.contains(RichTextEditor.FORMAT_UNDERLINE));
-				highlightSelectedItem(underline);
-			}
-		});
+  private void setupUnderline() {
+    ImageButton underline = view.findViewById(R.id.action_underline);
 
-		underline.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Toast.makeText(getContext(), R.string.toast_underline, Toast.LENGTH_SHORT).show();
-				return true;
-			}
-		});
-	}
+    underline.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.underline(richTextEditor.contains(RichTextEditor.FORMAT_UNDERLINE));
+        highlightSelectedItem(underline);
+      }
+    });
 
-	private void setupStrikethrough() {
-		ImageButton strikethrough = view.findViewById(R.id.action_strikethrough);
+    underline.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View v) {
+        Toast.makeText(getContext(), R.string.toast_underline, Toast.LENGTH_SHORT).show();
+        return true;
+      }
+    });
+  }
 
-		strikethrough.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.strikethrough(richTextEditor.contains(RichTextEditor.FORMAT_STRIKETHROUGH));
-				highlightSelectedItem(strikethrough);
-			}
-		});
+  private void setupStrikethrough() {
+    ImageButton strikethrough = view.findViewById(R.id.action_strikethrough);
 
-		strikethrough.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Toast.makeText(getContext(), R.string.toast_strikethrough, Toast.LENGTH_SHORT).show();
-				return true;
-			}
-		});
-	}
+    strikethrough.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.strikethrough(richTextEditor.contains(RichTextEditor.FORMAT_STRIKETHROUGH));
+        highlightSelectedItem(strikethrough);
+      }
+    });
 
-	private void setupBullet() {
-		ImageButton bullet = view.findViewById(R.id.action_insert_bullets);
+    strikethrough.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View v) {
+        Toast.makeText(getContext(), R.string.toast_strikethrough, Toast.LENGTH_SHORT).show();
+        return true;
+      }
+    });
+  }
 
-		bullet.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.bullet(richTextEditor.contains(RichTextEditor.FORMAT_BULLET));
-				highlightSelectedItem(bullet);
-			}
-		});
+  private void setupBullet() {
+    ImageButton bullet = view.findViewById(R.id.action_insert_bullets);
+
+    bullet.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.bullet(richTextEditor.contains(RichTextEditor.FORMAT_BULLET));
+        highlightSelectedItem(bullet);
+      }
+    });
 
 
-		bullet.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Toast.makeText(getContext(), R.string.toast_bullet, Toast.LENGTH_SHORT).show();
-				return true;
-			}
-		});
-	}
+    bullet.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View v) {
+        Toast.makeText(getContext(), R.string.toast_bullet, Toast.LENGTH_SHORT).show();
+        return true;
+      }
+    });
+  }
 
-	private void setupQuote() {
-		ImageButton quote = view.findViewById(R.id.action_quote);
+  private void setupQuote() {
+    ImageButton quote = view.findViewById(R.id.action_quote);
 
-		quote.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.quote(richTextEditor.contains(RichTextEditor.FORMAT_QUOTE));
-				highlightSelectedItem(quote);
-			}
-		});
+    quote.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.quote(richTextEditor.contains(RichTextEditor.FORMAT_QUOTE));
+        highlightSelectedItem(quote);
+      }
+    });
 
-		quote.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Toast.makeText(getContext(), R.string.toast_quote, Toast.LENGTH_SHORT).show();
-				return true;
-			}
-		});
-	}
+    quote.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View v) {
+        Toast.makeText(getContext(), R.string.toast_quote, Toast.LENGTH_SHORT).show();
+        return true;
+      }
+    });
+  }
 
-	private void setupAlignment() {
-		ImageButton alignLeft = view.findViewById(R.id.action_alignLeft);
+  private void setupAlignment() {
+    ImageButton alignLeft = view.findViewById(R.id.action_alignLeft);
 
-		alignLeft.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.alignLeft();
-				highlightSelectedItem(alignLeft);
-			}
-		});
+    alignLeft.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.alignLeft();
+        highlightSelectedItem(alignLeft);
+      }
+    });
 
-		alignLeft.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Toast.makeText(getContext(), R.string.toast_alignLeft, Toast.LENGTH_SHORT).show();
-				return true;
-			}
-		});
+    alignLeft.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View v) {
+        Toast.makeText(getContext(), R.string.toast_alignLeft, Toast.LENGTH_SHORT).show();
+        return true;
+      }
+    });
 
-		ImageButton alignRight = view.findViewById(R.id.action_alignRight);
+    ImageButton alignRight = view.findViewById(R.id.action_alignRight);
 
-		alignRight.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.alignRight();
-				highlightSelectedItem(alignRight);
-			}
-		});
+    alignRight.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.alignRight();
+        highlightSelectedItem(alignRight);
+      }
+    });
 
-		alignRight.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Toast.makeText(getContext(), R.string.toast_alignRight, Toast.LENGTH_SHORT).show();
-				return true;
-			}
-		});
+    alignRight.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View v) {
+        Toast.makeText(getContext(), R.string.toast_alignRight, Toast.LENGTH_SHORT).show();
+        return true;
+      }
+    });
 
-		ImageButton alignCenter = view.findViewById(R.id.action_alignCenter);
+    ImageButton alignCenter = view.findViewById(R.id.action_alignCenter);
 
-		alignCenter.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				richTextEditor.alignCenter();
-				highlightSelectedItem(alignCenter);
-			}
-		});
+    alignCenter.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        richTextEditor.alignCenter();
+        highlightSelectedItem(alignCenter);
+      }
+    });
 
-		alignCenter.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				Toast.makeText(getContext(), R.string.toast_alignCenter, Toast.LENGTH_SHORT).show();
-				return true;
-			}
-		});
-	}
+    alignCenter.setOnLongClickListener(new View.OnLongClickListener() {
+      @Override
+      public boolean onLongClick(View v) {
+        Toast.makeText(getContext(), R.string.toast_alignCenter, Toast.LENGTH_SHORT).show();
+        return true;
+      }
+    });
+  }
 
-	private void initSlidingButtons() {
-		View rightArrowView = view.findViewById(R.id.slidebar_right);
-		View leftArrowView = view.findViewById(R.id.slidebar_left);
-		View scrollView = view.findViewById(R.id.scroll_view);
+  private void initSlidingButtons() {
+    View rightArrowView = view.findViewById(R.id.slidebar_right);
+    View leftArrowView = view.findViewById(R.id.slidebar_left);
+    View scrollView = view.findViewById(R.id.scroll_view);
 
-		scrollView.getViewTreeObserver()
-			.addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
-				@Override
-				public void onScrollChanged() {
-					if (scrollView.canScrollHorizontally(-1)) {
-						leftArrowView.setVisibility(View.VISIBLE);
-					} else {
-						leftArrowView.setVisibility(View.GONE);
-					}
-					if (scrollView.canScrollHorizontally(1)) {
-						rightArrowView.setVisibility(View.VISIBLE);
-					} else {
-						rightArrowView.setVisibility(View.GONE);
-					}
-				}
-			});
+    scrollView.getViewTreeObserver()
+        .addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+          @Override
+          public void onScrollChanged() {
+            if (scrollView.canScrollHorizontally(-1)) {
+              leftArrowView.setVisibility(View.VISIBLE);
+            } else {
+              leftArrowView.setVisibility(View.GONE);
+            }
+            if (scrollView.canScrollHorizontally(1)) {
+              rightArrowView.setVisibility(View.VISIBLE);
+            } else {
+              rightArrowView.setVisibility(View.GONE);
+            }
+          }
+        });
 
-		rightArrowView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				scrollView.scrollTo((int) scrollView.getX() + 100, 0);
-			}
-		});
+    rightArrowView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        scrollView.scrollTo((int) scrollView.getX() + 100, 0);
+      }
+    });
 
-		leftArrowView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				scrollView.scrollTo((int) (scrollView.getX() - 100), 0);
-			}
-		});
+    leftArrowView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        scrollView.scrollTo((int) (scrollView.getX() - 100), 0);
+      }
+    });
 
-	}
+  }
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		String text = Html.toHtml(richTextEditor.getText(), Html.TO_HTML_PARAGRAPH_LINES_INDIVIDUAL);
-		if (text.length() != 0) {
-			noteModel.updateNote(note, text);
-		} else {
-			noteModel.deleteNote(noteId);
-		}
-	}
+  @Override
+  public void onPause() {
+    super.onPause();
+    String text = Html.toHtml(richTextEditor.getText(), Html.TO_HTML_PARAGRAPH_LINES_INDIVIDUAL);
+    String rawText = Jsoup.parse(text).text();
+    if (rawText.length() != 0) {
+      noteModel.updateNote(note, text);
+    } else {
+      noteModel.deleteNote(note.getId());
+    }
+  }
 
 }
