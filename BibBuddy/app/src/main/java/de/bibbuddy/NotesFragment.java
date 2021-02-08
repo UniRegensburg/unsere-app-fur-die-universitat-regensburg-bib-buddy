f3de5b9b00765ec672d135d258399907d85e2fe2
Sabrina Freisleben <41908872+xSabrina@users.noreply.github.com>
2021-02-08 13:19:49 +0100

    Setup select and delete multiple note items
    Add feature to select listview items in NotesFragment, which highlights them  by backgroundColorChange.
    On select at least one item a panel is shown on the bottom (bottom-up animation) and hidden (by bottom-down-animation) whenever selected items are removed or all items are deselected.
    The panel contains a ImageButton (delete-icon) to remove all selected items from the database and the recyclerViewList.

--- BibBuddy/app/src/main/java/de/bibbuddy/NotesFragment.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/NotesFragment.java
@@ -15,97 +15,70 @@ import java.util.List;
 
 public class NotesFragment extends Fragment {
 
-	static List<Note> notes;
-	private View view;
-	private RecyclerViewAdapter adapter;
-	private RecyclerView recyclerView;
+  static List<Note> notes;
+  private View view;
+  private RecyclerViewAdapter adapter;
+  private RecyclerView recyclerView;
 
-	@Nullable
-	@Override
-	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-													 @Nullable Bundle savedInstanceState) {
-		view = inflater.inflate(R.layout.fragment_notes, container, false);
-		recyclerView = view.findViewById(R.id.recyclerView);
-		NoteModel noteModel = new NoteModel(getContext());
-		notes = noteModel.getAllNotes();
-		setupRecyclerView();
-		enableSwipeToDelete();
-		setupAddButton();
-		return view;
-	}
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                           @Nullable Bundle savedInstanceState) {
+    view = inflater.inflate(R.layout.fragment_notes, container, false);
+    recyclerView = view.findViewById(R.id.recyclerView);
+    NoteModel noteModel = new NoteModel(getContext());
+    notes = noteModel.getAllNotes();
+    setupRecyclerView();
+    enableSwipeToDelete();
+    setupAddButton();
+    return view;
+  }
 
-	/*
-			Set up default RecyclerViewAdapter to manage recyclerview containing the notes arraylist
-	 */
-	private void setupRecyclerView() {
-		adapter = new RecyclerViewAdapter(notes, (MainActivity) getActivity());
-		recyclerView.setAdapter(adapter);
-        /*
-        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+  private void setupRecyclerView() {
+    adapter = new RecyclerViewAdapter(notes, (MainActivity) getActivity());
+    recyclerView.setAdapter(adapter);
+  }
 
-            @Override
-            public void onDraw(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
-                super.onDraw(c, parent, state);
-                SwipeToDeleteCallback.onDraw(c);
-            }
-        });
-    */
-	}
+  private void enableSwipeToDelete() {
+    SwipeToDeleteCallback swipeToDeleteCallback =
+        new SwipeToDeleteCallback(getContext(), adapter, (MainActivity) getActivity());
+    ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
+    itemTouchhelper.attachToRecyclerView(recyclerView);
+  }
 
-	/*
-			Enable swipe left to delete a recyclerView item
-			 by using ItemTouchHelper given a default SwipeToDeleteCallback-class
-	 */
-	private void enableSwipeToDelete() {
-		SwipeToDeleteCallback swipeToDeleteCallback =
-			new SwipeToDeleteCallback(getContext(), adapter, (MainActivity) getActivity());
-		ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
-		itemTouchhelper.attachToRecyclerView(recyclerView);
-	}
+  /*
+     TODO: Take this add button to single book view / book fragment
+  */
+  private void setupAddButton() {
+    View addButtonView = view.findViewById(R.id.addButton);
+    PopupMenu pm = new PopupMenu(getContext(), addButtonView);
+    pm.getMenuInflater().inflate(R.menu.add_note_menu, pm.getMenu());
 
-	/*
-		 TODO: Take this add button to single book view / book fragment
-	*/
-	private void setupAddButton() {
-		View addButtonView = view.findViewById(R.id.addButton);
-		PopupMenu pm = new PopupMenu(getContext(), addButtonView);
-		pm.getMenuInflater().inflate(R.menu.add_note_menu, pm.getMenu());
+    pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+      @Override
+      public boolean onMenuItemClick(MenuItem item) {
+        if (item.getItemId() == R.id.add_text_note) {
+          TextNoteEditorFragment nextFrag = new TextNoteEditorFragment();
+          getActivity().getSupportFragmentManager().beginTransaction()
+              .replace(R.id.fragment_container_view, nextFrag, "fragment_text_note_editor")
+              .addToBackStack(null)
+              .commit();
+        }
+        //TODO: add features to add voice notes and pictures
+        //case R.id.add_voice_note:
+        //case R.id.add_picture:
+        return true;
+      }
+    });
 
-		pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-			@Override
-			public boolean onMenuItemClick(MenuItem item) {
-				if (item.getItemId() == R.id.add_text_note) {
-					TextNoteEditorFragment nextFrag = new TextNoteEditorFragment();
-					getActivity().getSupportFragmentManager().beginTransaction()
-						.replace(R.id.fragment_container_view, nextFrag, "fragment_text_note_editor")
-						.addToBackStack(null)
-						.commit();
+    addButtonView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        pm.show();
+      }
+    });
 
-                    /*
-                        TODO: add features to add voice notes and pictures
+  }
 
-                    case R.id.add_voice_note:
-                        break;
-
-                    case R.id.add_picture:
-                        break;
-                     */
-
-				}
-				return true;
-			}
-		});
-
-        /*popup menu for choosing note type to add
-            -> maybe there is a more suitable UI form?
-         */
-		addButtonView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				pm.show();
-			}
-		});
-
-	}
 
 }
--- BibBuddy/app/src/main/java/de/bibbuddy/RecyclerViewAdapter.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/RecyclerViewAdapter.java
@@ -1,16 +1,22 @@
 package de.bibbuddy;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
@@ -19,117 +25,237 @@ import org.jsoup.Jsoup;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
 
-	private final List<Note> data;
-	private final MainActivity activity;
+  private final MainActivity activity;
+  private final List<Note> data;
+  private final NoteModel noteModel;
+  private Drawable background;
+  private ImageButton panelDelete;
+  private RelativeLayout hiddenDeletePanel;
+  private ViewGroup parent;
 
-	public RecyclerViewAdapter(List<Note> data, MainActivity activity) {
-		this.data = data;
-		this.activity = activity;
-		data.sort(new Comparator<Note>() {
-			@Override
-			public int compare(Note o1, Note o2) {
-				if (o1.getModDate() == null || o2.getModDate() == null) {
-					return 0;
-				}
-				return o1.getModDate().compareTo(o2.getModDate());
-			}
-		});
-		Collections.reverse(data);
-	}
+  /**
+   * Adapter constructor to connect a NoteList with the activity.
+   *
+   * @param data     List of notes as data content for the adapter
+   * @param activity Base activity
+   */
+  public RecyclerViewAdapter(List<Note> data, MainActivity activity) {
+    this.data = data;
+    this.activity = activity;
+    noteModel = new NoteModel(activity.getBaseContext());
+    data.sort(new Comparator<Note>() {
+      @Override
+      public int compare(Note o1, Note o2) {
+        if (o1.getModDate() == null || o2.getModDate() == null) {
+          return 0;
+        }
+        return o1.getModDate().compareTo(o2.getModDate());
+      }
+    });
+    Collections.reverse(data);
+  }
 
-	@NonNull
-	@Override
-	public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+  @NonNull
+  @Override
+  public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View itemView;
+    this.parent = parent;
+    Context context = parent.getContext();
+    itemView =
+        LayoutInflater.from(context).inflate(R.layout.list_view_item_note, parent, false);
+    background = itemView.getBackground();
+    hiddenDeletePanel = parent.getRootView().findViewById(R.id.hidden_delete_panel);
+    panelDelete = hiddenDeletePanel.findViewById(R.id.panel_delete);
+    return new MyViewHolder(itemView);
+  }
 
-		View itemView;
-		Context context = parent.getContext();
-		itemView =
-			LayoutInflater.from(context).inflate(R.layout.list_view_item_note, parent, false);
-		return new MyViewHolder(itemView);
-	}
+  /**
+   * Method to setup the custom ViewHolder components
+   * Consider: if text has more than 40 characters, only first 35 characters are displayed as title
+   * and adding " ..." as indicator for a longer text
+   *
+   * @param holder   custom ViewHolder instance
+   * @param position adapterPosition of the viewHolder-item
+   */
+  @Override
+  public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    Long id = data.get(position).getId();
+    setupCardView(holder, position);
+    holder.itemView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        TextNoteEditorFragment nextFrag = new TextNoteEditorFragment();
+        Bundle args = new Bundle();
+        args.putLong("noteId", id);
+        nextFrag.setArguments(args);
+        activity.getSupportFragmentManager().beginTransaction()
+            .replace(R.id.fragment_container_view, nextFrag, "fragment_text_note_editor")
+            .addToBackStack(null)
+            .commit();
+      }
+    });
+    holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
 
-	/*
-			Set up item views by using note text as title
-			 -> if text has more than 40 characters, only the first 35 characters are displayed as title
-					and adding " ..." as indicator for a longer text
-	 */
-	@Override
-	public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-		Long id = data.get(position).getId();
-		setupCardView(holder, position);
-		holder.itemView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				TextNoteEditorFragment nextFrag = new TextNoteEditorFragment();
-				Bundle args = new Bundle();
-				args.putLong("noteId", id);
-				nextFrag.setArguments(args);
-				activity.getSupportFragmentManager().beginTransaction()
-					.replace(R.id.fragment_container_view, nextFrag, "fragment_text_note_editor")
-					.addToBackStack(null)
-					.commit();
-			}
-		});
-	}
+      @Override
+      public boolean onLongClick(View v) {
 
-	private void setupCardView(MyViewHolder holder, int position) {
-		String text = data.get(position).getText();
-		text = Jsoup.parse(text).text();
-		if (text.contains("\n")) {
-			text = text.substring(0, text.indexOf("\n")) + "...";
-		}
-		if (text.length() > 40) {
-			text = text.substring(0, 35) + " ...";
-		}
-		holder.title.setText(text);
-		String dateString = getDate(data.get(position).getModDate());
-		holder.modDate.setText(dateString);
-		holder.type.setImageDrawable(
-			ContextCompat.getDrawable(activity.getApplicationContext(), R.drawable.document));
-	}
+        // Below line is just like a safety check, because sometimes holder could be null,
+        // in that case, getAdapterPosition() will return RecyclerView.NO_POSITION
+        if (position == RecyclerView.NO_POSITION) {
+          return false;
+        }
+        if (!v.isSelected()) {
+          v.setSelected(true);
+          v.setBackgroundColor(activity.getColor(R.color.flirt_light));
+        } else {
+          v.setSelected(false);
+          v.setBackground(background);
+        }
+        slideUpDown();
+        return true;
+      }
+    });
+    setupDeleteListener();
+  }
 
-	private String getDate(Long date) {
-		Date d = new Date(date);
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+  private void setupDeleteListener() {
+    panelDelete.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        int itemNumber = parent.getChildCount();
+        ArrayList<Integer> idCounter = new ArrayList<>();
+        for (int i = 0; i < itemNumber; i++) {
+          if (parent.getChildAt(i).isSelected()) {
+            idCounter.add(i);
+          }
+        }
+        removeBackendDataAndViewItems(idCounter);
+        hidePanel();
+      }
+    });
+  }
 
-		String string = simpleDateFormat.format(d);
-		String day = string.substring(8, 10);
-		String month = string.substring(5, 7);
-		String year = string.substring(0, 4);
-		String time = string.substring(11, 16);
+  private void removeBackendDataAndViewItems(ArrayList<Integer> idCounter) {
+    for (int i = 0; i < idCounter.size(); i++) {
+      noteModel.deleteNote(data.get(i).getId());
+    }
+    int removed = 0;
+    for (int i = 0; i < idCounter.size(); i++) {
+      if (removed == 0) {
+        removeItem(idCounter.get(i));
+      } else {
+        removeItem(idCounter.get(i) - removed);
+      }
+      removed++;
+    }
+  }
 
-		string = day + "." + month + "." + year + " " + time + " Uhr";
+  private void setupCardView(MyViewHolder holder, int position) {
+    String text = data.get(position).getText();
+    text = Jsoup.parse(text).text();
+    if (text.contains("\n")) {
+      text = text.substring(0, text.indexOf("\n")) + "...";
+    }
+    if (text.length() > 40) {
+      text = text.substring(0, 35) + " ...";
+    }
+    holder.title.setText(text);
+    String dateString = getDate(data.get(position).getModDate());
+    holder.modDate.setText(dateString);
+    holder.type.setImageDrawable(
+        ContextCompat.getDrawable(activity.getApplicationContext(), R.drawable.document));
+  }
 
-		return string;
-	}
+  private String getDate(Long date) {
+    Date d = new Date(date);
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
 
-	@Override
-	public int getItemCount() {
-		return data.size();
-	}
+    String string = simpleDateFormat.format(d);
+    String day = string.substring(8, 10);
+    String month = string.substring(5, 7);
+    String year = string.substring(0, 4);
+    String time = string.substring(11, 16);
 
-	public List<Note> getData() {
-		return data;
-	}
+    string = day + "." + month + "." + year + " " + time + " Uhr";
 
-	public void removeItem(int position) {
-		data.remove(position);
-		notifyItemRemoved(position);
-	}
+    return string;
+  }
 
-	public static class MyViewHolder extends RecyclerView.ViewHolder {
+  @Override
+  public int getItemCount() {
+    return data.size();
+  }
 
-		public final TextView modDate;
-		private final TextView title;
-		private final ImageView type;
+  public List<Note> getData() {
+    return data;
+  }
 
-		public MyViewHolder(View itemView) {
-			super(itemView);
-			title = itemView.findViewById(R.id.noteTitle);
-			modDate = itemView.findViewById(R.id.noteModDate);
-			type = itemView.findViewById(R.id.noteType);
-		}
+  public void removeItem(int position) {
+    data.remove(position);
+    notifyItemRemoved(position);
+  }
 
-	}
+  /**
+   * Method to perform an upside-down animation for the deletePanel.
+   */
+  public void slideUpDown() {
+    if (anyItemSelected() && !isPanelShown()) {
+      // Show the panel
+      Animation bottomUp = AnimationUtils.loadAnimation(activity.getBaseContext(),
+          R.anim.bottom_up);
+
+      hiddenDeletePanel.startAnimation(bottomUp);
+      hiddenDeletePanel.setVisibility(View.VISIBLE);
+    } else if (!anyItemSelected() && isPanelShown()) {
+      hidePanel();
+    }
+    System.out.println(anyItemSelected());
+  }
+
+  private void hidePanel() {
+    // Hide the Panel
+    Animation bottomDown = AnimationUtils.loadAnimation(activity.getBaseContext(),
+        R.anim.bottom_down);
+
+    hiddenDeletePanel.startAnimation(bottomDown);
+    hiddenDeletePanel.setVisibility(View.GONE);
+  }
+
+  private boolean anyItemSelected() {
+    int itemNumber = parent.getChildCount();
+    for (int i = 0; i < itemNumber; i++) {
+      if (parent.getChildAt(i).isSelected()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isPanelShown() {
+    return hiddenDeletePanel.getVisibility() == View.VISIBLE;
+  }
+
+  /**
+   * Custom ViewHolder to fit the RecyclerView's cardViews.
+   */
+  public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+    public final TextView modDate;
+    private final TextView title;
+    private final ImageView type;
+
+    /**
+     * Custom ViewHolder constructor to setup its basic view.
+     *
+     * @param itemView View of the RecyclerView-item.
+     */
+    public MyViewHolder(View itemView) {
+      super(itemView);
+      title = itemView.findViewById(R.id.noteTitle);
+      modDate = itemView.findViewById(R.id.noteModDate);
+      type = itemView.findViewById(R.id.noteType);
+    }
+
+  }
 
 }
