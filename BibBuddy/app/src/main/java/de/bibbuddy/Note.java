44cccd995369655fe4f15aa0b19051165a017948
Sabrina Freisleben <41908872+xSabrina@users.noreply.github.com>
2021-02-10 20:31:21 +0100

    Remove double constructor
    Remove double constructor of Note-class.

--- BibBuddy/app/src/main/java/de/bibbuddy/Note.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/Note.java
@@ -61,13 +61,6 @@ public class Note {
         this.noteFileId = noteFileId;
     }
 
-    public Note(String name, Integer type, String text) {
-        this.name = name;
-        this.type = type;
-        this.text = text;
-        this.noteFileId = noteFileId;
-    }
-
 	public Note() {
 	}
 
