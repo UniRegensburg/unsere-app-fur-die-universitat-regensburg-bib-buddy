52db95869ebd280085634925bf8ecea63ad6d35b
Sabrina Freisleben <41908872+xSabrina@users.noreply.github.com>
2021-02-11 09:53:31 +0100

    Remove add-button from NotesFragment
    Remove legacy add-button from NotesFragment.

--- BibBuddy/app/src/main/java/de/bibbuddy/NotesFragment.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/NotesFragment.java
@@ -2,10 +2,8 @@ package de.bibbuddy;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.PopupMenu;
 
 import java.util.List;
 
@@ -32,7 +30,6 @@ public class NotesFragment extends Fragment {
     notes = noteModel.getNoteList();
     setupRecyclerView();
     enableSwipeToDelete();
-    setupAddButton();
     return view;
   }
 
@@ -47,40 +44,5 @@ public class NotesFragment extends Fragment {
     ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
     itemTouchhelper.attachToRecyclerView(recyclerView);
   }
-
-  /*
-     TODO: Take this add button to single book view / book fragment
-  */
-  private void setupAddButton() {
-    View addButtonView = view.findViewById(R.id.addButton);
-    PopupMenu pm = new PopupMenu(getContext(), addButtonView);
-    pm.getMenuInflater().inflate(R.menu.add_note_menu, pm.getMenu());
-
-    pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-      @Override
-      public boolean onMenuItemClick(MenuItem item) {
-        if (item.getItemId() == R.id.add_text_note) {
-          TextNoteEditorFragment nextFrag = new TextNoteEditorFragment();
-          getActivity().getSupportFragmentManager().beginTransaction()
-              .replace(R.id.fragment_container_view, nextFrag, "fragment_text_note_editor")
-              .addToBackStack(null)
-              .commit();
-        }
-        //TODO: add features to add voice notes and pictures
-        //case R.id.add_voice_note:
-        //case R.id.add_picture:
-        return true;
-      }
-    });
-
-    addButtonView.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        pm.show();
-      }
-    });
-
-  }
-
 
 }
