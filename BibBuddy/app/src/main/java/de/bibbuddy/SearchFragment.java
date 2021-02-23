144f116cc50b9a5306caebdbe18d3738a4942ea4
Luis Moßburger <lmossburger@t-online.de>
2021-02-23 09:33:09 +0100

    Implement isbn retrieval
    * Add error message if isbn not findable
    * implement isbnRetriever class to handle API connection and retrieval
    * alter searchFragment for search logic (will be altered by Claudia Schönherr later on)

--- BibBuddy/app/src/main/java/de/bibbuddy/SearchFragment.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/SearchFragment.java
@@ -1,25 +1,65 @@
 package de.bibbuddy;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 /**
  * The SearchFragment is responsible for the search of books.
  *
- * @author Claudia Schönherr
+ * @author Claudia Schönherr, Luis Moßburger
  */
 public class SearchFragment extends Fragment {
 
-  @Nullable
-  @Override
-  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                           @Nullable Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_search, container, false);
-    return view;
-  }
+   private TextInputLayout searchField;
+   private EditText searchFieldText;
+   private Thread thread;
+   private IsbnRetriever isbnRetriever;
+
+   @Nullable
+   @Override
+   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                            @Nullable Bundle savedInstanceState) {
+      View view = inflater.inflate(R.layout.fragment_search,container,false);
+      searchField = view.findViewById(R.id.searchField);
+      searchFieldText = searchField.getEditText();
+      searchFieldText.setOnKeyListener(new View.OnKeyListener() {
+         public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if (keyCode==KeyEvent.KEYCODE_ENTER) {
+               if (event.getAction() != KeyEvent.ACTION_DOWN) {
+                  isbnRetriever = new IsbnRetriever(searchFieldText.getText().toString());
+                  thread = new Thread(isbnRetriever);
+                  thread.start();
+                  try {
+                     thread.join();
+                  } catch (Exception e) {
+                     System.out.println(e);
+                  }
+                  //Retrieve metadata that was saved
+                  Book book = isbnRetriever.getBook();
+                  if (book != null) {
+                     // @ClaudiaSchönherr - continue here with book to UI implementation :)
+                     System.out.println(book.getTitle());
+                  } else {
+                     Toast.makeText(getActivity(), getString(R.string.isbn_not_found),
+                             Toast.LENGTH_LONG).show();
+                  }
+               }
+            }
+            return true;
+         }
+      });
+
+      return view;
+   }
 }
