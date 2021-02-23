b6f41077dde37890983b31f8a02df82ec4af4628
Luis Moßburger <lmossburger@t-online.de>
2021-02-23 12:22:29 +0100

    Refactoring & checkstyle correction
    Refactored code & corrected checkstyle errors.

--- BibBuddy/app/src/main/java/de/bibbuddy/ApiReader.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/ApiReader.java
@@ -7,39 +7,44 @@ import java.util.stream.Collectors;
 
 public class ApiReader implements Runnable {
 
-    private String metadata = null;
-    private String url;
+  private final String url;
+  private String metadata = null;
 
-    /**
-     * The ApiReader class reads and returns data from the API
-     *
-     * @author Luis Moßburger
-     *
-     * @param url        concrete url that should be sent to API
-     */
-    ApiReader(String url) {
-        this.url = url;
-    }
+  /**
+   * The ApiReader class reads and returns data from the API.
+   *
+   * @param url concrete URL that should be sent to API
+   * @author Luis Moßburger
+   */
+  ApiReader(String url) {
+    this.url = url;
+  }
 
-    public void run() {
-        URL metadataUrl;
-        BufferedReader in;
-        String metadata = "";
-        System.out.println(url);
-        try {
-            metadataUrl = new URL(url);
-            in = new BufferedReader(new InputStreamReader(metadataUrl.openStream()));
-            metadata = in.lines().collect(Collectors.joining());
-            in.close();
-            this.metadata = metadata;
-        } catch(Exception e) {
-            System.out.println("*** ERROR ***");
-            System.out.println(e);
-        }
-    }
+  /**
+   * Main method for the Runnable to start - read from API and return.
+   *
+   * @author Luis Moßburger
+   */
+  public void run() {
+    URL metadataUrl;
+    BufferedReader in;
+    String metadata = "";
+    System.out.println(url);
 
-    public String getMetadata() {
-        return metadata;
+    try {
+      metadataUrl = new URL(url);
+      in = new BufferedReader(new InputStreamReader(metadataUrl.openStream()));
+      metadata = in.lines().collect(Collectors.joining());
+      in.close();
+      this.metadata = metadata;
+    } catch (Exception e) {
+      System.out.println("*** ERROR ***");
+      System.out.println(e);
     }
+  }
+
+  public String getMetadata() {
+    return metadata;
+  }
 
 }
--- BibBuddy/app/src/main/java/de/bibbuddy/IsbnRetriever.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/IsbnRetriever.java
@@ -1,129 +1,119 @@
 package de.bibbuddy;
 
+import java.io.StringReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
 
-import java.net.*;
-import java.io.*;
-import java.util.stream.Collectors;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
 public class IsbnRetriever implements Runnable {
+  private final String apiUrl = "https://lod.b3kat.de/";
+  private final String apiXmlParameter = "?output=xml";
+  private final String isbnApi = apiUrl + "data/isbn/%s" + apiXmlParameter;
+  private final String isbn;
+  private Book book = null;
 
-    private final String apiUrl = "https://lod.b3kat.de/";
-    private final String apiXmlParameter = "?output=xml";
-    private final String isbnApi = apiUrl + "data/isbn/%s" + apiXmlParameter;
-    private Book book = null;
-    private String isbn;
+  /**
+   * The IsbnRetriever class connects to the API and returns metadata for an ISBN.
+   *
+   * @param isbn ISBN from which metadata should be returned
+   * @author Luis Moßburger
+   */
+  IsbnRetriever(String isbn) {
+    this.isbn = isbn;
+  }
 
-    /**
-     * The IsbnRetriever class connects to the API and returns metadata for an isbn
-     *
-     * @author Luis Moßburger
-     *
-     * @param isbn        isbn from which metadata should be returned
-     */
-    IsbnRetriever(String isbn) {
-        this.isbn = isbn;
+  private static Document loadXmlFromString(String xml) throws Exception {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    InputSource is = new InputSource(new StringReader(xml));
+    return builder.parse(is);
+  }
+
+  private Book createRecord(Document xmlMetadata) {
+    long id = 0L;
+    String isbn = xmlMetadata.getElementsByTagName("bibo:isbn").item(0).getTextContent();
+    String title = xmlMetadata.getElementsByTagName("dc:title").item(0).getTextContent();
+    String subttle = xmlMetadata.getElementsByTagName("dc:title").item(0).getTextContent();
+    int pubYear = Integer
+        .parseInt(xmlMetadata.getElementsByTagName("dcterms:issued").item(0).getTextContent());
+    String publisher =
+        xmlMetadata.getElementsByTagName("dcterms:publisher").item(0).getTextContent();
+    String volume = "";
+    String edition = "";
+    String addInfos = "";
+    int createDate = 0;
+    int modDate = 0;
+
+    Book book = new Book(id, isbn, title, subttle, pubYear, publisher, volume, edition, addInfos,
+        createDate, modDate);
+    return book;
+  }
+
+  /**
+   * Main method for the Runnable to start - read from API and resolve to metadata.
+   *
+   * @author Luis Moßburger
+   */
+  public void run() {
+    //initialize variables
+    Thread thread;
+    ApiReader apiReader;
+    String metadata = "";
+    Document xmlMetadata = null;
+    //Read from API with isbn (Thread)
+    apiReader = new ApiReader(String.format(this.isbnApi, this.isbn));
+    thread = new Thread(apiReader);
+    thread.start();
+    try {
+      thread.join();
+    } catch (Exception e) {
+      System.out.println(e);
     }
+    //Retrieve metadata that was saved
+    metadata = apiReader.getMetadata();
+    if (metadata != null) {
+      //parse xml
+      try {
+        xmlMetadata = loadXmlFromString(metadata);
+      } catch (Exception e) {
+        System.out.println(e);
+      }
 
-    private static Document loadXmlFromString(String xml) throws Exception {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        InputSource is = new InputSource(new StringReader(xml));
-        return builder.parse(is);
-    }
+      //extract url
+      Node sameAsNode = xmlMetadata.getElementsByTagName("owl:sameAs").item(0);
+      Element sameAsEl = (Element) sameAsNode;
+      String sameAsUrl = sameAsEl.getAttribute("rdf:resource");
+      String endUrl = this.apiUrl + "data/" + sameAsUrl.split(".de/")[1] + apiXmlParameter;
 
-    private Book createRecord(Document xmlMetadata) {
-        long id;
-        String isbn;
-        String title;
-        String subttle;
-        int pubYear;
-        String publisher;
-        String volume;
-        String edition;
-        String addInfos;
-        int createDate;
-        int modDate;
-
-        id = 0L;
-        isbn = xmlMetadata.getElementsByTagName("bibo:isbn").item(0).getTextContent();
-        title = xmlMetadata.getElementsByTagName("dc:title").item(0).getTextContent();
-        subttle = xmlMetadata.getElementsByTagName("dc:title").item(0).getTextContent();
-        pubYear = Integer.parseInt(xmlMetadata.getElementsByTagName("dcterms:issued").item(0).getTextContent());
-        publisher = xmlMetadata.getElementsByTagName("dcterms:publisher").item(0).getTextContent();
-        volume = "";
-        edition = "";
-        addInfos = "";
-        createDate = 0;
-        modDate = 0;
-
-        Book book = new Book(id, isbn, title, subttle, pubYear, publisher, volume, edition, addInfos, createDate, modDate);
-        return book;
-    }
-
-    public void run() {
-        //initialize variables
-        Thread thread;
-        ApiReader apiReader;
-        String metadata = "";
-        Document xmlMetadata = null;
-        //Read from API with isbn (Thread)
-        apiReader = new ApiReader(String.format(this.isbnApi, this.isbn));
-        thread = new Thread(apiReader);
-        thread.start();
+      //Read from API with bv-nr
+      apiReader = new ApiReader(endUrl);
+      thread = new Thread(apiReader);
+      thread.start();
+      try {
+        thread.join();
+      } catch (Exception e) {
+        System.out.println(e);
+      }
+      //Retrieve metadata that was saved
+      metadata = apiReader.getMetadata();
+      if (metadata != null) {
+        //parse xml
         try {
-            thread.join();
-        } catch(Exception e) {
-            System.out.println(e);
+          xmlMetadata = loadXmlFromString(metadata);
+        } catch (Exception e) {
+          System.out.println(e);
         }
-        //Retrieve metadata that was saved
-        metadata = apiReader.getMetadata();
-        if (metadata != null) {
-            //parse xml
-            try {
-                xmlMetadata = loadXmlFromString(metadata);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-
-            //extract url
-            Node sameAsNode = xmlMetadata.getElementsByTagName("owl:sameAs").item(0);
-            Element sameAsEl = (Element) sameAsNode;
-            String sameAsUrl = sameAsEl.getAttribute("rdf:resource");
-            String endUrl = this.apiUrl + "data/" + sameAsUrl.split(".de/")[1] + apiXmlParameter;
-
-            //Read from API with bv-nr
-            apiReader = new ApiReader(endUrl);
-            thread = new Thread(apiReader);
-            thread.start();
-            try {
-                thread.join();
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-            //Retrieve metadata that was saved
-            metadata = apiReader.getMetadata();
-            if (metadata != null) {
-                //parse xml
-                try {
-                    xmlMetadata = loadXmlFromString(metadata);
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
-                //create a record
-                book = createRecord(xmlMetadata);
-            }
-        }
+        //create a record
+        book = createRecord(xmlMetadata);
+      }
     }
+  }
 
-    public Book getBook() {
-        return book;
-    }
+  public Book getBook() {
+    return book;
+  }
 
 }
