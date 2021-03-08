10f22388685fb54b51e86c93d42574a7b2dbea22
Sabrina Freisleben <41908872+xSabrina@users.noreply.github.com>
2021-03-08 14:08:07 +0100

    Adjust the BookNotesRecyclerViewAdapter to handle voice note features
    Adjust the BookNotesRecyclerViewAdapter to handle voice note features like play/pause and stop and showing an visualizer on playing audio.
    
    Also adjusted the cardView to match the general notes one in NotesFragment.
    
    Also adjust the reverse-chronological order to match the NotesFragment sorting and improve usability.
    
    Also rename inner holder-class to NotesViewHolder and adjust it to handle VoiceNote-view-items.

--- BibBuddy/app/src/main/java/de/bibbuddy/BookNotesRecyclerViewAdapter.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/BookNotesRecyclerViewAdapter.java
@@ -1,13 +1,25 @@
 package de.bibbuddy;
 
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
+import com.chibde.visualizer.LineBarVisualizer;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -16,62 +28,178 @@ import java.util.List;
  *
  * @author Sarah Kurek
  */
-public class BookNotesRecyclerViewAdapter extends
-    RecyclerView.Adapter<BookNotesRecyclerViewAdapter.BookNotesViewHolder> {
+public class BookNotesRecyclerViewAdapter
+    extends RecyclerView.Adapter<BookNotesRecyclerViewAdapter.NotesViewHolder> {
 
-  private final List<NoteItem> noteList;
-  private final BookNotesViewListener listener;
-  private final Context context;
+  private List<NoteItem> noteList;
+  private BookNotesViewListener listener;
+  private Context context;
+  private ArrayList<MediaPlayer> mediaPlayers;
+  private ArrayList<ImageButton> playButtons;
+  private ArrayList<LineBarVisualizer> visualizers;
+  private boolean paused;
+  private int mediaPlayerPosition;
 
   /**
    * Constructor for BookNotesRecyclerViewAdapter.
    *
    * @param noteList list of notes
    * @param listener listener for bookNotesView
-   * @param context context
+   * @param context  context
    */
   public BookNotesRecyclerViewAdapter(List<NoteItem> noteList, BookNotesViewListener listener,
                                       Context context) {
     this.noteList = noteList;
     this.listener = listener;
     this.context = context;
+    this.mediaPlayers = new ArrayList<>();
+    this.playButtons = new ArrayList<>();
+    this.visualizers = new ArrayList<>();
+    noteList.sort((o1, o2) -> {
+      if (o1.getModDate() == null || o2.getModDate() == null) {
+        return 0;
+      }
+      return o1.getModDate().compareTo(o2.getModDate());
+    });
+    Collections.reverse(noteList);
+  }
+
+  /**
+   * Custom ViewHolder constructor to setup its basic view.
+   *
+   * @param itemView View of the RecyclerView-item.
+   */
+  public BookNotesRecyclerViewAdapter(@NonNull View itemView) {
+    super();
   }
 
   @NonNull
   @Override
-  public BookNotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    return new BookNotesRecyclerViewAdapter.BookNotesViewHolder(
+  public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    return new NotesViewHolder(
         LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.list_view_item_book_notes_view, parent, false));
+            .inflate(R.layout.list_view_item_note, parent, false));
   }
 
   @Override
-  public void onBindViewHolder(@NonNull BookNotesViewHolder holder, int position) {
+  public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
     // Get element from your dataset at this position and replace the contents of the view
     // with that element
     NoteItem noteItem = noteList.get(position);
+    holder.getModDateView().setText(noteItem.getModDate());
+    holder.getNameView().setText(noteItem.getName());
+    holder.getTypeView().setImageResource(noteItem.getImage());
 
-    holder.getTextView().setText(noteItem.getName());
-    holder.getImageView().setImageResource(noteItem.getImage());
+    setupMediaButtons(holder, noteItem);
 
-    holder.itemView.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        listener.onItemClicked(position);
+    holder.itemView.setOnClickListener(v -> listener.onItemClicked(position));
+
+    holder.itemView.setOnLongClickListener(v -> {
+      if (position == RecyclerView.NO_POSITION) {
+        return false;
       }
+
+      listener.onLongItemClicked(position, noteItem, v);
+      return true;
     });
 
-    holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-      @Override
-      public boolean onLongClick(View v) {
-        if (position == RecyclerView.NO_POSITION) {
-          return false;
+    setupMediaButtons(holder, noteItem);
+  }
+
+  private void setupMediaButtons(NotesViewHolder holder, NoteItem noteItem) {
+    if (noteItem.getImage() == R.drawable.microphone) {
+      ImageButton playButton = holder.getPlayNoteButton();
+      playButtons.add(playButton);
+      playButton.setVisibility(View.VISIBLE);
+      ImageButton stopButton = holder.getStopNoteButton();
+      stopButton.setVisibility(View.VISIBLE);
+
+      MediaPlayer mediaPlayer = new MediaPlayer();
+      mediaPlayers.add(mediaPlayer);
+      LineBarVisualizer visualizer = holder.getVisualizer();
+      visualizers.add(visualizer);
+      setupMediaPlayerListeners(mediaPlayer, playButton, stopButton, visualizer, noteItem);
+    }
+  }
+
+  private void setupMediaPlayerListeners(MediaPlayer mediaPlayer, ImageButton playButton,
+                                         ImageButton stopButton, LineBarVisualizer visualizer,
+                                         NoteItem noteItem) {
+    visualizer.setColor(ContextCompat.getColor(context, R.color.design_default_color_secondary));
+    visualizer.setDensity(30);
+    visualizer.setPlayer(mediaPlayer.getAudioSessionId());
+    mediaPlayer.setOnCompletionListener(mp -> {
+      mp.reset();
+      paused = false;
+      setSelection(playButton, false, R.drawable.icon_play);
+      visualizer.setVisibility(View.INVISIBLE);
+    });
+
+    playButton.setOnClickListener(v -> {
+      if (v.isSelected()) {
+        ImageButton button = (ImageButton) v;
+        button.setImageResource(R.drawable.icon_play);
+        mediaPlayer.pause();
+        if (paused) {
+          mediaPlayerPosition = mediaPlayer.getCurrentPosition();
+          mediaPlayer.seekTo(mediaPlayerPosition);
+          useVisualizer(mediaPlayer, visualizer);
+          mediaPlayer.start();
         }
-
-        listener.onLongItemClicked(position, noteItem, v);
-        return true;
+        paused = !paused;
+      } else {
+        for (int i = 0; i < mediaPlayers.size(); i++) {
+          mediaPlayers.get(i).stop();
+          setSelection(playButtons.get(i), false, R.drawable.icon_play);
+          visualizers.get(i).setVisibility(View.INVISIBLE);
+        }
+        byte[] bytes = BookNotesViewModel.getNoteMedia(noteItem.getId());
+        playAudio(mediaPlayer, visualizer, bytes);
+        setSelection((ImageButton) v, true, R.drawable.icon_pause);
+        paused = false;
       }
     });
+
+    stopButton.setOnClickListener(v -> {
+      mediaPlayer.stop();
+      visualizer.setVisibility(View.INVISIBLE);
+      paused = false;
+    });
+  }
+
+  private void useVisualizer(MediaPlayer mediaPlayer, LineBarVisualizer visualizer){
+    int audioSessionId = mediaPlayer.getAudioSessionId();
+    if (audioSessionId != -1) {
+      visualizer.setVisibility(View.VISIBLE);
+      visualizer.setPlayer(mediaPlayer.getAudioSessionId());
+    }
+  }
+
+  private void setSelection(ImageButton button, boolean bool, int drawable) {
+    button.setSelected(bool);
+    button.setImageResource(drawable);
+  }
+
+  private void playAudio(MediaPlayer mediaPlayer, LineBarVisualizer visualizer, byte[] bytes) {
+    try {
+      File tempAudio =
+          File.createTempFile(String.valueOf(R.string.temporary_audio_file), String.valueOf(
+              R.string.audio_file_suffix), context.getCacheDir());
+      tempAudio.deleteOnExit();
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        Path path = Paths.get(tempAudio.getPath());
+        Files.write(path, bytes);
+      }
+      mediaPlayer.reset();
+      mediaPlayer.setDataSource(tempAudio.getPath());
+      mediaPlayer.prepareAsync();
+      mediaPlayer.setOnPreparedListener(mp -> {
+        useVisualizer(mp, visualizer);
+        mp.start();
+      });
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
   }
 
   @Override
@@ -85,30 +213,53 @@ public class BookNotesRecyclerViewAdapter extends
     void onLongItemClicked(int position, NoteItem noteItem, View v);
   }
 
-  public static class BookNotesViewHolder extends RecyclerView.ViewHolder {
+  public static class NotesViewHolder extends RecyclerView.ViewHolder {
 
-    private final TextView textView;
-    private final ImageView imageView;
+    public final TextView modDate;
+    private final TextView name;
+    private final ImageView type;
+    private final ImageButton play;
+    private final ImageButton stop;
+    private final LineBarVisualizer visualizer;
 
     /**
-     * Custom ViewHolder constructor to setup basic view.
+     * Custom ViewHolder constructor to setup its basic view.
      *
-     * @param itemView current item of the view
+     * @param itemView View of the RecyclerView-item.
      */
-    public BookNotesViewHolder(@NonNull View itemView) {
+    public NotesViewHolder(View itemView) {
       super(itemView);
-
-      textView = itemView.findViewById(R.id.item_name_book_notes);
-      imageView = itemView.findViewById(R.id.note_icon);
-      // TODO: Differentiation Text, audio, image
+      modDate = itemView.findViewById(R.id.noteModDate);
+      name = itemView.findViewById(R.id.noteName);
+      type = itemView.findViewById(R.id.noteType);
+      play = itemView.findViewById(R.id.play_note);
+      stop = itemView.findViewById(R.id.stop_note);
+      visualizer = itemView.findViewById(R.id.notesVisualizer);
     }
 
-    public TextView getTextView() {
-      return textView;
+    public TextView getModDateView() {
+      return modDate;
     }
 
-    public ImageView getImageView() {
-      return imageView;
+    public TextView getNameView() {
+      return name;
+    }
+
+    public ImageView getTypeView() {
+      return type;
+    }
+
+    public ImageButton getPlayNoteButton() {
+      return play;
+    }
+
+    public ImageButton getStopNoteButton() {
+      return stop;
+    }
+
+    public LineBarVisualizer getVisualizer() {
+      return visualizer;
     }
   }
+
 }
