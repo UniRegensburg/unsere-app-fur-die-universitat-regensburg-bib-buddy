d50160c28600ce6306636ba23003f612f1a13513
Sabrina Freisleben <41908872+xSabrina@users.noreply.github.com>
2021-03-23 19:17:21 +0100

    Checkstyle

--- BibBuddy/app/src/main/java/de/bibbuddy/NotesFragment.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/NotesFragment.java
@@ -17,7 +17,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.util.List;
 
 /**
--- BibBuddy/app/src/main/java/de/bibbuddy/TextNoteEditorFragment.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/TextNoteEditorFragment.java
@@ -17,7 +17,6 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
