0ef4fa084dbabbd0d56de3c93b3e228a1868059e
Sabrina Freisleben <41908872+xSabrina@users.noreply.github.com>
2021-03-31 16:16:00 +0200

    Clean up NoteItem.class
    Clean up NoteItem.class by removing a legacy param for the noteText and its getter-method.

--- BibBuddy/app/src/main/java/de/bibbuddy/NoteItem.java
+++ BibBuddy/app/src/main/java/de/bibbuddy/NoteItem.java
@@ -10,12 +10,10 @@ public class NoteItem implements SortableItem {
 
   private final Long modDate;
   private final String name;
-  private final String text;
   private final int image;
   private final Long id;
   private final Long bookId;
-
-  private String modDateStr;
+  private final String modDateStr;
 
   /**
    * Constructor to set up a NoteItem for view/UI usages.
@@ -26,27 +24,13 @@ public class NoteItem implements SortableItem {
    * @param id      id of the note
    * @param bookId  id of the book that the note is connected to
    */
-  public NoteItem(Long modDate, String name, String text, int image, Long id, Long bookId) {
+  public NoteItem(Long modDate, String name, int image, Long id, Long bookId) {
     this.modDate = modDate;
     this.modDateStr = DateConverter.convertDateToString(modDate);
     this.name = name;
-    this.text = text;
     this.image = image;
     this.id = id;
     this.bookId = bookId;
-  }
-
-  /**
-   * Constructor to set up a NoteItem for view/UI usages.
-   *
-   * @param modDate string-value of the modification date of the note
-   * @param name    name of the note
-   * @param image   id for the drawable resource of the note type icon
-   * @param id      id of the note
-   */
-  public NoteItem(Long modDate, String name, int image, Long id, Long bookId) {
-    this(modDate, name, null, image, id, bookId);
-    this.modDateStr = DateConverter.convertDateToString(modDate);
   }
 
   @Override
@@ -69,10 +53,6 @@ public class NoteItem implements SortableItem {
 
   public Long getBookId() {
     return bookId;
-  }
-
-  public String getNoteText() {
-    return text;
   }
 
   public String getModDateStr() {
